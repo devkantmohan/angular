a1626fe1b7023c3954172aa785ce9cdfc48f099c
Arpit Jain <arpit.ja@HCL.COM>
2017-03-09 10:37:33 +0530

    add routing and dashboard pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {detailscomp} from './app.detailscomp';
+
+import { DashboardComponent }   from './dashboard.component';
+import{plancomp} from './plan.component';
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,detailscomp,DashboardComponent,plancomp
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
